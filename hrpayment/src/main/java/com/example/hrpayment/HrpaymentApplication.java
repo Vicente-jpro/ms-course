89bfd4d50dd8793ccc939bc3fcad2c52bfad9e30
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-06-10 10:18:18 +0100

    payment ms created

---
+++ hrpayment/src/main/java/com/example/hrpayment/HrpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrpaymentApplication.class, args);
+	}
+
+}
