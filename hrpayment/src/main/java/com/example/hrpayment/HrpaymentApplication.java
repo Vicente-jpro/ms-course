14f06bd1bf56392638b9e87917576dca70ea70dd
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-06-12 00:12:28 +0100

    LoadBalance added

--- hrpayment/src/main/java/com/example/hrpayment/HrpaymentApplication.java
+++ hrpayment/src/main/java/com/example/hrpayment/HrpaymentApplication.java
@@ -2,11 +2,10 @@ package com.example.hrpayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "hrworkers")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrpaymentApplication {
--- hrworkers/src/main/java/com/example/hrworkers/HrworkersApplication.java
+++ hrworkers/src/main/java/com/example/hrworkers/HrworkersApplication.java
@@ -2,7 +2,9 @@ package com.example.hrworkers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrworkersApplication {
 
