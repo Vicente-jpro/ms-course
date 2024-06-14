1fc64858e58d6964389bf19960fcc01c128649e3
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-06-15 00:37:31 +0100

    Config server created

---
+++ hr-config-server/src/test/java/com/example/htconfigserver/HtConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
