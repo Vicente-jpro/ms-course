9807da67a98c220577697e0a7570640ada3b015e
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-06-15 17:39:19 +0100

    Atuator added

--- hrworkers/src/main/java/com/example/hrworkers/HrworkersApplication.java
+++ hrworkers/src/main/java/com/example/hrworkers/HrworkersApplication.java
@@ -4,12 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+/* Colocamos aqui o @RefreshScope porque estamos acessar as configuracoes
+apartir de @Value("${test.config}")
+*/
+@RefreshScope 
 @EnableEurekaClient
 @SpringBootApplication
 public class HrworkersApplication {
