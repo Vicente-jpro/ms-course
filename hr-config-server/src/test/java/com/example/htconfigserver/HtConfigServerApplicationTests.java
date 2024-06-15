aaf34b7e5219b0eb9682a75d4d10b4eee9e2238e
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-06-15 12:44:46 +0100

    Bug fixed

--- hr-config-server/src/test/java/com/example/htconfigserver/HtConfigServerApplicationTests.java
+++ hr-config-server/src/test/java/com/example/htconfigserver/HtConfigServerApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.htconfigserver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HtConfigServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
--- hrworkers/src/main/java/com/example/hrworkers/HrworkersApplication.java
+++ hrworkers/src/main/java/com/example/hrworkers/HrworkersApplication.java
@@ -1,15 +1,32 @@
 package com.example.hrworkers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableEurekaClient
 @SpringBootApplication
 public class HrworkersApplication {
 
+	@Value("${test.config}")
+	private String profileType;
+	
+	@Bean
+	public CommandLineRunner create() {
+		return args ->{
+			log.info("############## PROFILE TYPE: {} ##############",profileType);
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HrworkersApplication.class, args);
+	
+
 	}
 
 }
